19ad070e14e37d44f954b175e84e232485eccb35
Fabio Burzigotti <fburzigo@redhat.com>
2025-03-03 21:12:58 +0100

    [add-it] - (Try and) Make RestaurantAgent behavior (questions) deterministic

--- src/main/java/org/agenticai/restaurant/RestaurantAgent.java
+++ src/main/java/org/agenticai/restaurant/RestaurantAgent.java
@@ -11,7 +11,7 @@ import org.agenticai.restaurant.weather.WeatherService;
 @RegisterAiService(modelName = "tool-use")
 @SystemMessage("""
         You are an AI dealing with the booking for a restaurant.
-        Do not invent the customer name or party size, but explicitly ask for them if not provided. 
+        Do not guess the customer name or the party size, but explicitly ask for such information if not provided. 
         If the user specifies a preference (indoor/outdoor), you should book the table with the preference.  However, please check the weather forecast before booking the table.
         """)
 @SessionScoped
@@ -22,7 +22,8 @@ public interface RestaurantAgent {
             You receive request from customer and need to book their table in the restaurant.
             Please be polite and try to handle the user request.
             
-            Before booking the table, makes sure to have valid date for the reservation, and that the user explicitly provided his name and party size.
+            Before booking the table, makes sure to have valid date for the reservation, and that you asked the customer name and the number of participants to the party, which must be provided by the customer explicitly.
+            When asking for the customer name, you can only use the following sentence, "Which name should be used for the reservation?", and nothing else.
             If the booking is successful just notify the user.
             
             Today is: {current_date}.
