8a9b806eab9ff42f9b1e06b540b80798d319f0f3
mariofusco <mario.fusco@gmail.com>
2025-03-12 08:47:50 +0100

    bump versions

--- src/main/java/org/agenticai/routing/CategoryRouter.java
+++ src/main/java/org/agenticai/routing/CategoryRouter.java
@@ -4,14 +4,15 @@ import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.ApplicationScoped;
 
-@RegisterAiService
+@RegisterAiService(chatMemoryProviderSupplier = RegisterAiService.NoChatMemoryProviderSupplier.class)
+@ApplicationScoped
 public interface CategoryRouter {
 
     @UserMessage("""
-            Analyze the following user request and categorize it as 'legal', 'medical', 'technical'
-            or 'unknown' in case the request doesn't belong to any of those categories.
+            Analyze the following user request and categorize it as 'legal', 'medical' or 'technical'.
+            In case the request doesn't belong to any of those categories categorize it as 'unknown'.
             Reply with only one of those words and nothing else.
-            The user request is {request}.
+            The user request is: '{request}'.
             """)
     RequestCategory classify(String request);
 
